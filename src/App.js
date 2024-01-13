7b3e7e7b535443cc0640efbb9111f0ec1c2573ce
muzi55 <muzi5@kakao.com>
2024-01-13 11:57:31 +0900

    카운트앱

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import "./App.css";
+import { useState } from 'react'
+import './App.css'
 
 function App() {
-  return <></>;
+    const [count, setCount] = useState(0)
+    const [value, setValue] = useState(0)
+
+    const increase = () => {
+        setCount(count + +value)
+    }
+    const decrease = () => setCount((prev) => prev - +value)
+
+    const handleOnchangeInput = (e) => {
+        setValue(e.target.value)
+    }
+
+    return (
+        <>
+            <h1>나의 첫번째 카운트 앱</h1>
+            {count}
+            <div>
+                <button onClick={increase}>+</button>
+                <button onClick={decrease}>-</button>
+            </div>
+            <input type="number" name="" id="" onChange={handleOnchangeInput} value={value} />
+        </>
+    )
 }
 
-export default App;
+export default App
